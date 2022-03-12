e9a21491ea59acbaf6d2a5770a95d09797d37a7b
ysdy44 <ysdy44@outlook.com>
2022-03-12 19:56:45 +0800

    Add App

--- Paint Keyboard/App.xaml.cs
+++ Paint Keyboard/App.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System.Windows;
+﻿using Paint_Keyboard.Model;
+using System.Windows;
 
 namespace Paint_Keyboard
 {
     public partial class App : Application
     {
+        public static Solution Solution;
         public App()
         {
+            App.Solution = XML.ConstructSolutionFile();
         }
     }
 }
