49aaf4042425dc9aea4646b51ddbdef658d32186
ysdy44 <ysdy44@outlook.com>
2022-03-12 19:42:49 +0800

    Add PopupBase

---
+++ Paint Keyboard/Elements/PopupBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Paint_Keyboard.Elements
+{
+    public class PopupBase : Popup
+    {
+        public void Hide() => base.IsOpen = false;
+        public void Show(Vector position)
+        {
+            this.Move(position);
+            base.IsOpen = true;
+        }
+        public void Move(Vector position)
+        {
+            base.HorizontalOffset = position.X;
+            base.VerticalOffset = position.Y;
+        }
+    }
+}
