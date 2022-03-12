ce39a917f3953161b6af612ac3afc8077121a573
ysdy44 <ysdy44@outlook.com>
2022-03-12 19:50:29 +0800

    Add IconListView

---
+++ Paint Keyboard/Controls/IconListView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Paint_Keyboard.Elements;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace Paint_Keyboard.Controls
+{
+    public partial class IconListView : ListView
+    {
+
+        private readonly IList<int> IconFontItems = IconFontCharacter.CreateSegoeUISymbolCharacterCodesList().ToList();
+
+        public int SelectedIcon
+        {
+            get
+            {
+                int index = base.SelectedIndex;
+                if (index < 0) return 0;
+                return this.IconFontItems[index];
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    base.SelectedIndex = -1;
+                    base.Focus();
+                }
+                else
+                {
+                    for (int i = 0; i < this.IconFontItems.Count; i++)
+                    {
+                        int item = this.IconFontItems[i];
+                        if (item == value)
+                        {
+                            base.SelectedIndex = i;
+                            base.Focus();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public IconListView()
+        {
+            this.InitializeComponent();
+            base.ItemsSource = this.IconFontItems;
+        }
+
+    }
+}
---
+++ Paint Keyboard/Controls/ItemIcon.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Paint_Keyboard.Elements;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Paint_Keyboard.Controls
+{
+    public partial class ItemIcon : UserControl
+    {
+
+        #region DependencyProperty
+
+
+        /// <summary> Gets or sets the Icon of<see cref = "ItemIcon" />. </summary>
+        public int Icon
+        {
+            get => (int)base.GetValue(IconProperty);
+            set => base.SetValue(IconProperty, value);
+        }
+        /// <summary> Identifies the <see cref = "ItemIcon.Icon" /> dependency property. </summary>
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon), typeof(int), typeof(ItemIcon), new PropertyMetadata(1, (sender, e) =>
+        {
+            ItemIcon control = (ItemIcon)sender;
+
+            if (e.NewValue is int value)
+            {
+                control.GlyphTextBlock.Text = value.ToGlyph();
+                control.CodeKeyTextBlock.Text = value.ToCodeKey();
+            }
+        }));
+
+
+        #endregion
+
+        //@Construct
+        /// <summary>
+        /// Initializes a ItemIcon. 
+        /// </summary> 
+        public ItemIcon()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
