c7be425f1e7ad5a4621659e434d2d1d45d922caf
ysdy44 <ysdy44@outlook.com>
2022-03-12 19:45:23 +0800

    Add StringList

---
+++ Paint Keyboard/Model/StringList.cs
@@ -0,0 +1,18 @@
+﻿using Paint_Keyboard.Strings;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Paint_Keyboard.Model
+{
+    public class StringList : Dictionary<LangCode, string>
+    {
+        public StringList() { }
+        public StringList(IDictionary<LangCode, string> dictionary) : base(dictionary) { }
+
+        public StringList Clone() => new StringList(this.ToDictionary
+        (
+            item => item.Key,
+            item => item.Value
+        ));
+    }
+}
