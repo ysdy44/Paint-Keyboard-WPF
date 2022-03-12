5f42e1d540606d7991d86f750b5bbaba1867e129
ysdy44 <ysdy44@outlook.com>
2022-03-12 19:46:26 +0800

    Add Solution

---
+++ Paint Keyboard/Model/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Paint_Keyboard.Model
+{
+    public sealed class Solution
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public IEnumerable<Item> Items { get; set; }
+    }
+}
