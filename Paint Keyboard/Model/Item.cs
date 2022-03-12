111dc6f5877762c50df5d0a7ede5a2a652f798cf
ysdy44 <ysdy44@outlook.com>
2022-03-12 19:46:06 +0800

    Add Item

---
+++ Paint Keyboard/Model/Item.cs
@@ -0,0 +1,20 @@
+﻿using Paint_Keyboard.Strings;
+
+namespace Paint_Keyboard.Model
+{
+    public class Item
+    {
+        public StringList Strings { get; set; }
+        public KeyList Keys { get; set; }
+        public int Icon { get; set; }
+        public string Title { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public string GetTitle(LangCode code)
+        {
+            if (this.Strings != null) return this.Strings[code];
+            else if (this.Title == null) return string.Empty;
+            else return this.Title;
+        }
+    }
+}
